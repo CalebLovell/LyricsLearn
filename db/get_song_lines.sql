28e0e46ded487597b1990ec284413301cdd611d9
CalebLovell <39709791+CalebLovell@users.noreply.github.com>
2019-04-23 22:55:03 -0600

    song improvements and dummy song

--- db/get_song_lines.sql
+++ db/get_song_lines.sql
@@ -1,4 +1,4 @@
-select line_lyrics, line_explanation, line_index
+select line_lyrics, line_explanation, line_index, li.line_id
 from lines li
 join song_instances si on si.song_instance_id = li.song_instance_id
 where si.song_instance_id = $1;
--- src/Components/Song/Song.jsx
+++ src/Components/Song/Song.jsx
@@ -17,8 +17,11 @@ class Song extends Component {
       songTranslationLanguages: [],
       songLines: [],
       songTranslation: [],
-      visibleExplanation: ""
+      visibleExplanation: "",
+      // lineID: "",
+      // transID: ""
     };
+    // this.slot = React.createRef();
   }
 
   componentDidMount() {
@@ -29,6 +32,8 @@ class Song extends Component {
     try {
       const { songID } = this.props.match.params;
       const result = await axios.get(`/song/${songID}`);
+      console.log(result.data)
+      // console.log(result.data.songLines[5].line_id);
       const {
         song_instance_title,
         song_instance_art,
@@ -58,7 +63,7 @@ class Song extends Component {
     try {
       const { songID } = this.props.match.params;
       const result = await axios.get(`/song/${songID}/${languageID}`);
-      console.log(result);
+      console.log(result.data.songTranslation[5].line_id);
       this.setState({
         songTranslation: result.data.songTranslation
       });
@@ -73,27 +78,62 @@ class Song extends Component {
     this.setState({
       visibleExplanation: explanation
     });
+    console.log(this.slot)
   };
+
+  // highlight = (lineID) => {
+  //   style="red"
+  // }
+
+  // setLineID = (lineID) => {
+  //   this.setState({
+  //     lineID
+  //   })
+  //   console.log(lineID)
+  // }
+
+  // setTransID = (transID) => {
+  //   this.setState({
+  //     transID
+  //   })
+  //   console.log(transID)
+  // }
 
   render() {
     let mappedLyrics = this.state.songLines.map((line, i) => {
+      
       return (
         <div
           className="line-slot"
+          // ref={this.slot}
           key={i}
           onClick={() => this.setExplanation(line.line_explanation)}
+          // onMouseOver={() => this.highlight(line.line_id)}
+          // style={(this.state.lineID === this.state.transID) ? {color: '#fc4a1a'} : {}}
         >
           <p className="line-lyrics">{line.line_lyrics}</p>
         </div>
       );
     });
-    let mappedTranslation = this.state.songTranslation.map((line, i) => {
-      return (
-        <div className="line-slot" key={i} onClick={() => this.setExplanation(line.line_translation_explanation)}>
-          <p className="line-lyrics">{line.line_translation_lyrics}</p>
-        </div>
-      );
-    });
+    let mappedTranslation = this.state.songTranslation.map(
+      (translatedLine, i) => {
+        return (
+          <div
+            className="line-slot"
+            // ref={this.slot}
+            key={i}
+            onClick={() =>
+              this.setExplanation(translatedLine.line_translation_explanation)
+            }
+            // onMouseOver={() => this.setTransID(translatedLine.line_id)}
+          >
+            <p className="line-lyrics">
+              {translatedLine.line_translation_lyrics}
+            </p>
+          </div>
+        );
+      }
+    );
     let mappedLanguages = this.state.songTranslationLanguages.map(
       (translationLanguage, i) => {
         return (
@@ -149,7 +189,7 @@ class Song extends Component {
           </div>
           <div className="explanation-box">
             <div className="explanation">
-              <p>Explanation:  {this.state.visibleExplanation}</p>
+              <p>{this.state.visibleExplanation}</p>
             </div>
           </div>
         </div>
@@ -164,3 +204,23 @@ export default connect(
   mapState,
   { getUserData }
 )(Song);
+
+// const slot = document.getElementsByClassName("line-slot")
+// const leftSlot = slot.getElementsByClassName("left")[i];
+// const rightSlot = slot.getElementsByClassName("right")[i];
+// leftSlot.onmouseover = function() {
+//   leftSlot.style.color = "red";
+//   rightSlot.style.color = "red";
+// }
+// leftSlot.onmouseout = function() {
+//   leftSlot.style.color = "red";
+//   rightSlot.style.color = "white";
+// };
+// rightSlot.onmouseover = function() {
+//   leftSlot.style.color = "red";
+//   rightSlot.style.color = "red";
+// }
+// rightSlot.onmouseout = function() {
+//   leftSlot.style.color = "white";
+//   rightSlot.style.color = "white";
+// };
